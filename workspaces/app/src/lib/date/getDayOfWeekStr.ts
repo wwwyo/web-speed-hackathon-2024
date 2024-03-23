820754e07bbe358666f4fb0ff7da6cc219969144
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-03-23 14:43:58 +0900

    feat: moment削除

--- workspaces/app/src/lib/date/getDayOfWeekStr.ts
+++ workspaces/app/src/lib/date/getDayOfWeekStr.ts
@@ -1,10 +1,8 @@
-import type moment from 'moment-timezone';
-
 const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'] as const;
 
-export const getDayOfWeekStr = (date: moment.Moment) => {
-  const dayOfWeek = date.day();
-  const dayStr = days.at(dayOfWeek);
+export const getDayOfWeekStr = (date: Date) => {
+  const dayOfWeek = date.getDay();
+  const dayStr = days[dayOfWeek];
   if (dayStr == null) {
     throw new Error('dayOfWeek is invalid');
   }
