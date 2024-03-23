3304eb14512efe58b823563455e350e3024c863b
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-03-23 16:50:04 +0900

    feat: 画像formatをavifに変更

--- workspaces/app/src/foundation/hooks/useImagev2.ts
+++ workspaces/app/src/foundation/hooks/useImagev2.ts
@@ -3,7 +3,7 @@ import { getImageUrl } from '../../lib/image/getImageUrl';
 export const useImageV2 = ({ height, imageId, width }: { height: number; imageId: string; width: number }) => {
   const dpr = window.devicePixelRatio;
   return getImageUrl({
-    format: 'webp',
+    format: 'avif',
     height: height * dpr,
     imageId,
     width: width * dpr,
