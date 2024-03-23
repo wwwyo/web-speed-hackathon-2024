ab9b8fed0f0d78c2b82a76602506c98a0d6fbd03
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-03-24 04:56:02 +0900

    chore: remove 画像圧縮

--- workspaces/server/src/routes/image/index.ts
+++ workspaces/server/src/routes/image/index.ts
@@ -1,21 +1,13 @@
 import { createReadStream } from 'node:fs';
 import type { ReadStream } from 'node:fs';
-import fs from 'node:fs/promises';
 import path from 'node:path';
 
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { HTTPException } from 'hono/http-exception';
-import { Image } from 'image-js';
 import { z } from 'zod';
 
 import { IMAGES_PATH } from '../../constants/paths';
-import type { ConverterInterface } from '../../image-converters/ConverterInterface';
-import { avifConverter } from '../../image-converters/avifConverter';
-import { jpegConverter } from '../../image-converters/jpegConverter';
-import { jpegXlConverter } from '../../image-converters/jpegXlConverter';
-import { pngConverter } from '../../image-converters/pngConverter';
-import { webpConverter } from '../../image-converters/webpConverter';
 
 const createStreamBody = (stream: ReadStream) => {
   const body = new ReadableStream({
@@ -52,15 +44,6 @@ const IMAGE_MIME_TYPE: Record<SupportedImageExtension, string> = {
   ['webp']: 'image/webp',
 };
 
-const IMAGE_CONVERTER: Record<SupportedImageExtension, ConverterInterface> = {
-  ['avif']: avifConverter,
-  ['jpeg']: jpegConverter,
-  ['jpg']: jpegConverter,
-  ['jxl']: jpegXlConverter,
-  ['png']: pngConverter,
-  ['webp']: webpConverter,
-};
-
 const app = new Hono();
 
 app.get(
@@ -82,15 +65,7 @@ app.get(
   async (c) => {
     const { globby } = await import('globby');
 
-    const { ext: reqImgExt, name: reqImgId } = path.parse(c.req.valid('param').imageFile);
-
-    const resImgFormat = c.req.valid('query').format ?? reqImgExt.slice(1);
-
-    if (!isSupportedImageFormat(resImgFormat)) {
-      throw new HTTPException(501, { message: `Image format: ${resImgFormat} is not supported.` });
-    }
-
-    const origFileGlob = [path.resolve(IMAGES_PATH, `${reqImgId}`), path.resolve(IMAGES_PATH, `${reqImgId}.*`)];
+    const origFileGlob = [path.resolve(IMAGES_PATH, `${c.req.valid('param').imageFile}.*`)];
     const [origFilePath] = await globby(origFileGlob, { absolute: true, onlyFiles: true });
     if (origFilePath == null) {
       throw new HTTPException(404, { message: 'Not found.' });
@@ -100,33 +75,10 @@ app.get(
     if (!isSupportedImageFormat(origImgFormat)) {
       throw new HTTPException(500, { message: 'Failed to load image.' });
     }
-    if (resImgFormat === origImgFormat && c.req.valid('query').width == null && c.req.valid('query').height == null) {
-      // 画像変換せずにそのまま返す
-      c.header('Content-Type', IMAGE_MIME_TYPE[resImgFormat]);
-      return c.body(createStreamBody(createReadStream(origFilePath)));
-    }
 
-    const origBinary = await fs.readFile(origFilePath);
-    const image = new Image(await IMAGE_CONVERTER[origImgFormat].decode(origBinary));
-
-    const reqImageSize = c.req.valid('query');
-
-    const scale = Math.max((reqImageSize.width ?? 0) / image.width, (reqImageSize.height ?? 0) / image.height) || 1;
-    const manipulated = image.resize({
-      height: Math.ceil(image.height * scale),
-      preserveAspectRatio: true,
-      width: Math.ceil(image.width * scale),
-    });
-
-    const resBinary = await IMAGE_CONVERTER[resImgFormat].encode({
-      colorSpace: 'srgb',
-      data: new Uint8ClampedArray(manipulated.data),
-      height: manipulated.height,
-      width: manipulated.width,
-    });
-
-    c.header('Content-Type', IMAGE_MIME_TYPE[resImgFormat]);
-    return c.body(resBinary);
+    // 圧縮せずにストリームでそのまま返す
+    c.header('Content-Type', IMAGE_MIME_TYPE[origImgFormat as SupportedImageExtension]);
+    return c.body(createStreamBody(createReadStream(origFilePath)));
   },
 );
 
